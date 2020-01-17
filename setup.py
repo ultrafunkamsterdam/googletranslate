8f4bbecac4d1a01a16cfba37bb2949737027ed2d
Leon <ultrafunkamsterdam@users.noreply.github.com>
2020-01-17 09:21:25 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(
+    name='googletranslate',
+    version='1.1',
+    packages=['googletranslate'],
+    url='https://github.com/ultrafunkamsterdam/googletranslate',
+    license='MIT',
+    author='UltrafunkAmsterdam',
+    author_email='',
+    description='Google Translate module using the reverse engineered public API (free)'
+)
