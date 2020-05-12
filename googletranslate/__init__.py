85a2df7ee60b99f51bd1beb7673ebd6b122db5b2
Leon <ultrafunkamsterdam@users.noreply.github.com>
2020-05-12 20:14:51 +0200

    Update __init__.py

--- googletranslate/__init__.py
+++ googletranslate/__init__.py
@@ -203,7 +203,7 @@ class Translator(object):
 
     _ua = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
            "AppleWebKit/537.36 (KHTML, like Gecko) "
-           "Chrome/75.0.3770.142 Safari/537.36")
+           "Chrome/81.0.4044.138 Safari/537.36")
     _api_url = "https://translate.google.com{}"
 
     def __init__(self,
