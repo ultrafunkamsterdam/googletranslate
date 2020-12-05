ff933c8696564a6afb06c41a381cb09003d31de4
Leon <ultrafunkamsterdam@users.noreply.github.com>
2020-12-05 09:10:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='googletranslate',
-    version='1.1',
+    version='2.0',
     packages=['googletranslate'],
     url='https://github.com/ultrafunkamsterdam/googletranslate',
     license='MIT',
